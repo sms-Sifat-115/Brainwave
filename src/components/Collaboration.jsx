56c5fbe6ff31d23b92441e9b03c81d67af9b810a
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-10-04 07:54:15 +0600

    11th Draft: Services Section Added

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -11,7 +11,7 @@ const Collaboration = () => {
 
   const handleButtonClick = () => {
     setRotate(true);
-    setTimeout(() => setRotate(false), 20000); // Stop rotation after 30 seconds
+    setTimeout(() => setRotate(false), 20000);
   };
 
   return (
@@ -44,51 +44,53 @@ const Collaboration = () => {
             {collabText}
           </p>
 
-          <div
-            className={`relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100 parent-container ${
-              rotate ? styles["rotate-animation"] : ""
-            }`}
-          >
-            <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
-              <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
-                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
-                  <img
-                    src={brainwaveSymbol}
-                    width={48}
-                    height={48}
-                    alt="brainwave"
-                  />
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+            <div
+              className={`relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100 parent-container ${
+                rotate ? styles["rotate-animation"] : ""
+              }`}
+            >
+              <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+                <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+                  <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                    <img
+                      src={brainwaveSymbol}
+                      width={48}
+                      height={48}
+                      alt="brainwave"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <ul>
-              {collabApps.map((app, index) => (
-                <li
-                  key={app.id}
-                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
-                    index * 45
-                  }`}
-                >
-                  <div
-                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+              <ul>
+                {collabApps.map((app, index) => (
+                  <li
+                    key={app.id}
+                    className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
                       index * 45
                     }`}
                   >
-                    <img
-                      className="m-auto"
-                      width={app.width}
-                      height={app.height}
-                      alt={app.title}
-                      src={app.icon}
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
+                    <div
+                      className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                        index * 45
+                      }`}
+                    >
+                      <img
+                        className="m-auto"
+                        width={app.width}
+                        height={app.height}
+                        alt={app.title}
+                        src={app.icon}
+                      />
+                    </div>
+                  </li>
+                ))}
+              </ul>
 
-            <LeftCurve />
-            <RightCurve />
+              <LeftCurve />
+              <RightCurve />
+            </div>
           </div>
         </div>
       </div>
@@ -97,3 +99,5 @@ const Collaboration = () => {
 };
 
 export default Collaboration;
+
+//
